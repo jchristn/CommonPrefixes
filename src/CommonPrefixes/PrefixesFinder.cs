df54888339d642f7c7ef7d1322a81081e218b547
Joel Christner <joel.christner@gmail.com>
2023-11-30 15:26:55 -0800

    NuGet v1.0.2, .net8, add string variant

--- src/CommonPrefixes/PrefixesFinder.cs
+++ src/CommonPrefixes/PrefixesFinder.cs
@@ -41,7 +41,42 @@ namespace CommonPrefixes
                     string prefix = string.Empty;
 
                     for (int i = 0; i < (split.Length - 1); i++)
-                    { 
+                    {
+                        prefix += split[i] + delimiter;
+                    }
+
+                    ret.Add(prefix);
+                }
+            }
+
+            return ret.Distinct().ToList();
+        }
+
+        /// <summary>
+        /// Generate a list of common prefixes found within a supplied list of strings and a given delimiter character.
+        /// </summary>
+        /// <param name="vals">List of strings.</param>
+        /// <param name="delimiter">Delimiter string.</param>
+        /// <returns>List of strings.</returns>
+#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+        public static List<string> Find(List<string> vals, string? delimiter)
+#pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+        {
+            if (vals == null || vals.Count < 1) return new List<string>();
+
+            List<string> ret = new List<string>();
+
+            foreach (string val in vals)
+            {
+                if (String.IsNullOrEmpty(val)) continue;
+
+                string[] split = val.Split(delimiter);
+                if (split != null && split.Length > 1)
+                {
+                    string prefix = string.Empty;
+
+                    for (int i = 0; i < (split.Length - 1); i++)
+                    {
                         prefix += split[i] + delimiter;
                     }
 
--- src/Test/Program.cs
+++ src/Test/Program.cs
@@ -6,7 +6,7 @@ namespace Test
 {
     public static class Program
     {
-        public static List<string> _Values = new List<string>
+        public static List<string> _Values1 = new List<string>
         {
             "/foo/",
             "/foo/file1",
@@ -21,9 +21,30 @@ namespace Test
             "/foo/bar/barbar/file1",
             "/foo/bar/barbar/file2"
         };
+
+        public static List<string> _Values2 = new List<string>
+        {
+            "::foo::",
+            "::foo::file1",
+            "::foo::file2",
+            "::foo::bar::",
+            "::foo::bar::file1",
+            "::foo::bar::file2",
+            "bar",
+            "::baz::",
+            "::baz::file1",
+            "::baz::file2",
+            "::foo::bar::barbar::file1",
+            "::foo::bar::barbar::file2"
+        };
+
         public static void Main(string[] args)
         {
-            List<string> prefixes = PrefixesFinder.Find(_Values, '/');
+            List<string> prefixes;
+
+            Console.WriteLine("");
+            Console.WriteLine("Find by character");
+            prefixes = PrefixesFinder.Find(_Values1, '/');
             if (prefixes != null)
             {
                 foreach (string prefix in prefixes)
@@ -33,6 +54,22 @@ namespace Test
 
                 Console.WriteLine("");
                 Console.WriteLine(prefixes.Count + " common prefixes found");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Find by string");
+            prefixes = PrefixesFinder.Find(_Values2, "::");
+            if (prefixes != null)
+            {
+                foreach (string prefix in prefixes)
+                {
+                    Console.WriteLine(prefix);
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine(prefixes.Count + " common prefixes found");
+
+                Console.WriteLine("");
             }
         }
     }
